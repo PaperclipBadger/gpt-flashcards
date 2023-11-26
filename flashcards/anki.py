c0b72eba643df9d12b71e7fa71ac63006d5669d0
Blaine Rogers <blaine.w.rogers@gmail.com>
2023-11-26 16:33:04 +0900

    Pull more information out of .apkg

--- flashcards/anki.py
+++ flashcards/anki.py
@@ -8,14 +8,28 @@ import tempfile
 import zipfile
 
 
+@dataclasses.dataclass
+class Template:
+    name: str
+    qfmt: str
+    afmt: str
+
+
 @dataclasses.dataclass(frozen=True)
 class Model:
+    id: int
     name: str
     field_names: list[str]
+    css: str
+    latex_pre: str
+    latex_post: str
+    templates: list[Template]
 
 
 @dataclasses.dataclass(frozen=True)
 class Note:
+    id: int
+    guid: int
     model: Model
     field_contents: list[str]
     tags: set[str]
@@ -26,9 +40,24 @@ class Note:
 
 
 @dataclasses.dataclass(frozen=True)
+class Card:
+    id: int
+    note: Note
+    template: int
+
+
+@dataclasses.dataclass(frozen=True)
+class Deck:
+    id: int
+    name: str
+    cards: list[Card]
+
+
+@dataclasses.dataclass(frozen=True)
 class Collection:
     models: list[Model]
     notes: list[Note]
+    decks: list[Deck]
 
 
 def read_package(path: pathlib.Path) -> Collection:
@@ -40,20 +69,40 @@ def read_package(path: pathlib.Path) -> Collection:
 
         with sqlite3.connect(tmpdir / "collection.anki2") as conn:
             models_json, = conn.execute("SELECT models FROM col").fetchone()
-            notes_rows = conn.execute("SELECT mid,flds,tags FROM notes").fetchall()
+            decks_json, = conn.execute("SELECT decks FROM col").fetchone()
+            notes_rows = conn.execute("SELECT id,guid,mid,mod,flds,tags FROM notes").fetchall()
+            cards_rows = conn.execute("SELECT id,nid,did,ord FROM cards").fetchall()
 
     models = {}
     for mid, model in json.loads(models_json).items():
-        name = model["name"]
-        field_names = [fld["name"] for fld in model["flds"]]
-        models[int(mid)] = Model(name=name, field_names=field_names)
-
-    notes = []
-    for mid, flds, tags in notes_rows:
+        models[int(mid)] = Model(
+            id=mid,
+            name=model["name"],
+            field_names=[fld["name"] for fld in model["flds"]],
+            css=model["css"],
+            latex_pre=model["latexPre"],
+            latex_post=model["latexPost"],
+            templates=[
+                Template(name=t["name"], qfmt=t["qfmt"], afmt=t["afmt"])
+                for t in model["tmpls"]
+            ],
+        )
+    
+    notes = {}
+    for nid, guid, mid, mod, flds, tags in notes_rows:
         model = models[mid]
         fields = flds.split("\x1f")
         tags = set(tags.strip().split())
-        note = Note(model=model, field_contents=fields, tags=tags)
-        notes.append(note)
+        note = Note(id=int(nid), guid=guid, model=model, field_contents=fields, tags=tags)
+        notes[nid] = note
+
+    decks = {}
+    for deck in json.loads(decks_json).values():
+        name = deck["name"]
+        deck_id = deck["id"]
+        decks[deck_id] = Deck(id=deck_id, name=name, cards=[])
     
-    return Collection(models, notes)
+    for cid, nid, did, ord in cards_rows:
+        decks[did].cards.append(Card(id=cid, note=notes[int(nid)], template=int(ord)))
+    
+    return Collection(list(models.values()), list(notes.values()), list(decks.values()))
